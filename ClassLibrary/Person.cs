18acaa9fba3154d66c9f24120b64c8882a8c077f
Nikita Polivanchuk <ipz231_pnr@student.ztu.edu.ua>
2024-01-07 01:45:21 +0200

    Class Person added

---
+++ ClassLibrary/Person.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace ClassLibrary
+{
+    public class Person
+    {
+        protected string Name;
+        protected string Surname;
+        protected DateTime BirthDate;
+
+        public Person()
+        {
+            Name = string.Empty;
+            Surname = string.Empty;
+            BirthDate = DateTime.MinValue;
+        }
+
+        public Person(string name, string surname, DateTime birthDate)
+        {
+            Name = name;
+            Surname = surname;
+            BirthDate = birthDate;
+        }
+
+        public virtual string ShowInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append('[');
+            sb.Append("Name: "); sb.Append(Name);
+            sb.Append(", Surname: "); sb.Append(Surname);
+            sb.Append(", Birth date: "); sb.Append(BirthDate.ToShortDateString());
+            sb.Append(']');
+            return sb.ToString();
+        }
+    }
+}
