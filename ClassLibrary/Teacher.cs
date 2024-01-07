9508a4e41aaafa1a0dac2f39fd1fb732eda3fb19
Nikita Polivanchuk <ipz231_pnr@student.ztu.edu.ua>
2024-01-07 03:05:22 +0200

    Class Teacher added

---
+++ ClassLibrary/Teacher.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+
+namespace ClassLibrary
+{
+    public class Teacher : Person
+    {
+        protected string Position;
+        protected string Department;
+        protected string IHE;
+
+
+        public Teacher() : base()
+        {
+            Position = string.Empty;
+            Department = string.Empty;
+            IHE = string.Empty;
+        }
+        public Teacher(
+            string name,
+            string surname,
+            DateTime birthDate,
+            string position,
+            string department,
+            string iHE) : base(name, surname, birthDate)
+        {
+            Position = position;
+            Department = department;
+            IHE = iHE;
+        }
+        public Teacher(
+            Person person,
+            string position,
+            string department,
+            string iHE) : base(person)
+        {
+            Position = position;
+            Department = department;
+            IHE = iHE;
+        }
+
+        public override string ShowInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(base.ShowInfo());
+            sb.Append(", Position: "); sb.Append(Position);
+            sb.Append(", Department: "); sb.Append(Department);
+            sb.Append(", IHE: "); sb.Append(IHE);
+
+            return sb.ToString();
+        }
+    }
+}
