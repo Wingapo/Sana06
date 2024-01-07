48d456d7c4a169f3bd45e53bef5c211a13739812
Nikita Polivanchuk <ipz231_pnr@student.ztu.edu.ua>
2024-01-07 03:35:44 +0200

    ConsoleApp and Class LibraryUser added

---
+++ ClassLibrary/LibraryUser.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace ClassLibrary
+{
+    public class LibraryUser : Person
+    {
+        protected int TicketNumber;
+        DateTime IssueDate;
+        double MonthlyFee;
+
+        public LibraryUser() : base() 
+        {
+            TicketNumber = 0;
+            IssueDate = DateTime.MinValue;
+            MonthlyFee = 0;
+        }
+
+        public LibraryUser(
+            string name,
+            string surname,
+            DateTime birthDate,
+            int ticketNumber, 
+            DateTime issueDate, 
+            double monthlyFee) : base(name, surname, birthDate)
+        {
+            TicketNumber = ticketNumber;
+            IssueDate = issueDate;
+            MonthlyFee = monthlyFee;
+        }
+
+        public LibraryUser (
+            Person person,
+            int ticketNumber,
+            DateTime issueDate, 
+            double monthlyFee) : base(person)
+        {
+            TicketNumber = ticketNumber;
+            IssueDate = issueDate;
+            MonthlyFee = monthlyFee;
+        }
+
+        public override string ShowInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(base.ShowInfo());
+            sb.Append(", Ticket number: "); sb.Append(TicketNumber);
+            sb.Append(", Issue date: "); sb.Append(IssueDate.ToShortDateString());
+            sb.Append(", Monthly fee: "); sb.Append(MonthlyFee);
+            
+            return sb.ToString();
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Person[] people = new Person[5];
+        people[0] = new Person("name1", "surname1", new DateTime(1982, 3, 20));
+        people[1] = new Entrant("name2", "surname2", new DateTime(2006, 2, 11), 190, 11.2f, "school #1");
+        people[2] = new Student("name3", "surname3", new DateTime(2004, 3, 20), 2, "group", "fac", "ihe");
+        people[3] = new Teacher("name4", "surname4", new DateTime(1979, 8, 5), "pos", "depart", "ihe");
+        people[4] = new LibraryUser("name5", "surname5", new DateTime(2001, 12, 10), 6349458, new DateTime(2024, 1, 1), 100);
+
+        foreach (Person person in people)
+        {
+            Console.WriteLine(person.ShowInfo());
+        }
+    }
+}
